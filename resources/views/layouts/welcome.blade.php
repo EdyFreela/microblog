fbc80ae94b58829a1974164fd1cf1a37aff2b6dd
EdyFreela <31071891+EdyFreela@users.noreply.github.com>
2017-09-19 17:23:33 -0300

    Design Layout Landing Page

---
+++ resources/views/layouts/welcome.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>MicroBlog - Sistema de Blog com QRCode v0.1</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+    <style>
+        body {
+            font-family: 'Quicksand', sans-serif;
+        }
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+        .section-one, .section-two, .section-tree{
+            min-height: 100vh;
+        }
+        .section-one .container{
+            padding-top:200px;
+        }
+        .section-two{
+            background-color: #f1f1f1;
+        }
+        .section-tree{
+            
+        }
+        .section-footer{
+            background-color: #232525;
+            color: #999999;
+            padding-top: 40px;
+            padding-bottom: 40px;
+        }
+        .section-footer .links ul{margin:0;padding:0;}
+        .section-footer .links li{list-style: none; margin-bottom:10px;}
+        .section-footer .links a{color:#999999;}
+        .section-footer .endereco{}
+        .section-footer .social{}
+        .section-footer .copy{
+            margin-top:10px;
+            padding-top:40px;
+            border-top:1px solid #999999;
+        }
+
+    </style>
+    @yield('style')
+</head>
+<body id="app-layout">
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    MicroBlog
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    @if (Auth::guest())
+                    @else
+                        <li><a href="{{ url('/home') }}">Home</a></li>
+                    @endif
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Acesso</a></li>
+                        <li><a href="{{ url('/register') }}">Registro</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    @yield('content')
+
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    @yield('script')
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,130 @@
-@extends('layouts.app')
+@extends('layouts.welcome')
+
+@section('style')
+<style>
+nav.navbar{
+   -webkit-transition: all 0.8s ease;
+   transition: all 0.8s ease;
+}
+nav.navbar{
+    min-height: 70px;
+    padding-top: 10px;
+    background-color: transparent;    
+}
+nav.navbar.shrink {
+    min-height: 50px;
+    padding-top: 0px;
+    background-color: #232525;
+    color:#000;
+}
+</style>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
+<div class="section-one">
+    <div class="container">
+        <h1>Selo de segurança Bike Registrada</h1>
+        <h2>"Faça todo mundo saber que a sua bike é sua"</h2>
+        <h2>Frete único para todo Brasil!</h2>
+    </div>
+</div>
+<div class="section-two">
+    <div class="container">
+        <div class="col-md-12">
+            <h1>Faça seu pedido agora</h1>
+        </div>
+        <div class="col-md-6">
+            <h2>Dados pessoais</h2>
+            {!! Form::open(['url' => 'register']) !!}
+            <div class="col-md-6">
+                <div class="form-group form-group-lg">
+                    <label for="exampleInputEmail1">Nome</label>
+                    {!! Form::text('name', null, array('placeholder' => 'Nome','class' => 'form-control')) !!}
+                    <small id="emailHelp" class="form-text text-muted">máximo 20 caracteres.</small>
                 </div>
             </div>
+            <div class="col-md-6">
+                <div class="form-group form-group-lg">
+                    <label for="exampleInputEmail1">Sobrenome</label>
+                    {!! Form::text('lastname', null, array('placeholder' => 'Sobrenome','class' => 'form-control')) !!}
+                    <small id="emailHelp" class="form-text text-muted">máximo 20 caracteres.</small>
+                </div>
+            </div>            
+            <div class="col-md-12">
+                <div class="form-group form-group-lg">
+                    <label for="exampleInputEmail1">Seu melhor e-mail</label>
+                    {!! Form::text('email', null, array('placeholder' => 'E-Mail','class' => 'form-control')) !!}
+                    <small id="emailHelp" class="form-text text-muted">máximo 40 caracteres.</small>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group form-group-lg">
+                    <label for="exampleInputEmail1">Senha</label>
+                    {!! Form::password('password', array('placeholder' => 'Senha','class' => 'form-control')) !!}
+                    <small id="emailHelp" class="form-text text-muted">máximo 40 caracteres.</small>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group form-group-lg">
+                    <label for="exampleInputEmail1">Confirme a Senha</label>
+                    {!! Form::password('password-confirm', array('placeholder' => 'Confirmar Senha','class' => 'form-control')) !!}
+                    <small id="emailHelp" class="form-text text-muted">máximo 40 caracteres.</small>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group form-group-lg">
+                    {{ Form::button('Finalizar compra', ['type' => 'submit', 'class' => 'btn btn-danger btn-lg col-md-12'] )  }}
+                </div>
+            </div>            
+
+            {!! Form::close() !!}
+        </div>
+        <div class="col-md-6">
+            <h2>Endereço de entrega</h2>
+            
         </div>
     </div>
 </div>
+<div class="section-tree">
+    <div class="container">
+        <h1>Selo de segurança Bike Registrada</h1>
+        <p>O Selo de Segurança Bike Registrada é um produto tecnológico que além de desestimular a ação dos ladrões, facilita a identificação da bicicleta e do proprietário, reduzindo de forma significativa o risco da compra de uma bike roubada e ajudando na devolução de bicicletas recuperadas.</p>
+    </div>
+</div>
+<div class="section-footer">
+    <div class="container">
+        <div class="col-md-4 links">
+            <ul>
+                <li><a href="{{ url('/politica-de-privacidade')}}">Politica de privacidade</a></li>
+                <li><a href="{{ url('/termo-de-useo')}}">Termo de Uso</a></li>
+                <li><a href="">Link 01</a></li>
+            </ul>
+        </div>
+        <div class="col-md-4 endereco">
+            <p>Avenida Pau Brasil, lote 6, sala 1902, Centro</p>
+            <p>Empresarial E-Business, Águas Claras</p>
+            <p>71.916-500, Brasília-DF</p>
+            <p>+55 (61)3522-4521</p>
+            <p>contato@bikeregistrada.com.br</p>
+        </div>
+        <div class="col-md-4 social">
+            
+        </div>
+        <div class="col-md-12 copy">
+            <p>MicroBlog - Sistema de Blog com QRCode v0.1. Todos os direitos reservados {!! date('Y') !!}.</p>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('script')
+<script>
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+      } else {
+        $('nav').removeClass('shrink');
+      }
+    });    
+</script>
 @endsection
